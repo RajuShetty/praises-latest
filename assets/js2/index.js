5e0919f3b7a675772228c7b3c92463022d8529fb
Raju Shetty <rajushetty.b@gmail.com>
2017-06-15 11:52:44 +0530

    removed pushbots

--- assets/js2/index.js
+++ assets/js2/index.js
@@ -15,16 +15,7 @@ var app = {
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
-		window.plugins.PushbotsPlugin.initialize("58b931954a9efa92a18b4567", {"android":{"sender_id":"629172333031"}});
-		// Should be called once app receive the notification only while the application is open or in background
-		window.plugins.PushbotsPlugin.on("notification:received", function(data){
-			console.log("received:" + JSON.stringify(data));
-		});
 		
-		// Should be called once the notification is clicked
-		window.plugins.PushbotsPlugin.on("notification:clicked", function(data){
-			console.log("clicked:" + JSON.stringify(data));
-		});
 		
         app.receivedEvent('deviceready');
     },
