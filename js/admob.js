c71add1c22b6a78443233e8fbf3e9f9cfbb2faa3
Raju-mac <rajushetty.b@gmal.com>
2019-02-02 23:27:35 +0530

    new app

--- js/admob.js
+++ js/admob.js
@@ -4,8 +4,8 @@ var admobid = {};
 if( /(android)/i.test(navigator.userAgent) ) {
   admobid = { // for Android
     banner: '',
-    interstitial: 'ca-app-pub-6844692352099040/2565507990',
-    rewardvideo: 'ca-app-pub-6844692352099040/6350029700',
+    interstitial: 'ca-app-pub-7048516006218968/1295114039',
+    rewardvideo: 'ca-app-pub-7048516006218968/4152501221',
   };
 } else if(/(ipod|iphone|ipad)/i.test(navigator.userAgent)) {
   admobid = { // for iOS
